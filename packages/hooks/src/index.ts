abfff68130c687b6b66e431704c1a03e9c76e757
Rinat <rinatkins@gmail.com>
2025-07-07 20:10:25 +0300

    fix: return exports

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,3 +1,5 @@
 export { useAddress } from "./useAddress.js";
 export { useAddressInput } from "./useAddressInput.js";
+export { useBalance, useWatchBalance } from "./balance/index.js";
+export { useFetchNativeCurrencyPrice } from "./useFetchNativeCurrencyPrice.js";
 export { isENS } from "./utils/ens.js";
