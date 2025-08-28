fe7a26aaee47154433bc53037a7e874ebf9c5183
Shiv Bhonde <shivbhonde04@gmail.com>
2025-08-28 12:33:22 +0530

    make switchTheme float at right corner

--- example/app/ScaffoldETHProvider.tsx
+++ example/app/ScaffoldETHProvider.tsx
@@ -5,6 +5,7 @@ import { WagmiProvider } from "wagmi";
 import { Toaster } from "react-hot-toast";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { wagmiConfig } from "./wagmiConfig";
+import { SwitchTheme } from "./components/SwitchTheme";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,11 +20,12 @@ export const ScaffoldEthAppWithProviders = ({ children }: { children: React.Reac
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>
-          <div className="flex justify-end m-8">
+          <div className="flex justify-end h-full p-4 bg-[var(--color-sui-primary-neutral)]">
             <ConnectButton />
           </div>
 
           {children}
+          <SwitchTheme />
         </RainbowKitProvider>
       </QueryClientProvider>
       <Toaster />
