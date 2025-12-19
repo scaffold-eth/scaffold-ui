8f782c3a911c381f312369105740e7e4be23e8ba
Pablo <55535804+Pabl0cks@users.noreply.github.com>
2025-12-19 09:05:13 -0300

    docs: Add useBalance and useEtherInput hooks  (#69)

--- docs/vocs.config.ts
+++ docs/vocs.config.ts
@@ -36,6 +36,8 @@ export default defineConfig({
         { text: "Installation", link: "/hooks" },
         { text: "useAddress", link: "/hooks/useAddress" },
         { text: "useAddressInput", link: "/hooks/useAddressInput" },
+        { text: "useBalance", link: "/hooks/useBalance" },
+        { text: "useEtherInput", link: "/hooks/useEtherInput" },
       ],
     },
   ],
