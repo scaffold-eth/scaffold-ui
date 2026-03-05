ab727e0b48470e0b066132b5d2e91cbe1dbf7daf
web3buidlerz <seba@buidlerlabs.com>
2026-03-05 17:01:02 +0200

    fix: debug-contracts implementation to work wit scaffold hbar setup

--- packages/debug-contracts/src/index.ts
+++ packages/debug-contracts/src/index.ts
@@ -1,4 +1,4 @@
-export { Contract, type ContractProps } from "./Contract.js";
-export { useContractConfig, ContractConfigProvider, type ContractConfig } from "./contexts/ContractConfigContext.js";
-export { Bytes32Input, BytesInput, IntegerInput } from "./components/inputs/index.js";
-export { IntegerVariant } from "./utils/inputs.js";
+export { Contract, type ContractProps } from "./Contract";
+export { useContractConfig, ContractConfigProvider, type ContractConfig } from "./contexts/ContractConfigContext";
+export { Bytes32Input, BytesInput, IntegerInput } from "./components/inputs/index";
+export { IntegerVariant } from "./utils/inputs";
